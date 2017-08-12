8e687e7a1f943639c45d0870d095574e73727855
rahul-kumar-yadav <30690192+rahul-kumar-yadav@users.noreply.github.com>
2017-08-12 17:25:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,8 +5,8 @@ var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight () {
     marginLeft = marginLeft + 10;
-    image.style.marginLeft= marginLeft + 'px';
+    img.style.marginLeft = marginLeft + 'px';
 }
-img.onclick = function() {
+img.onclick = function () {
     var interval = setInterval(moveRight , 100);
 };
