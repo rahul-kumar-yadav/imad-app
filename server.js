02e311efd3c3b4814fd6d87531d4d78378a56365
rahul-kumar-yadav <30690192+rahul-kumar-yadav@users.noreply.github.com>
2017-08-12 16:53:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,6 +82,10 @@ app.get('/:articlename' , function (req,res) {
 });
 
 
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
