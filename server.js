3874a160718caa941c57d23f3c36c86d0c212142
rahul-kumar-yadav <30690192+rahul-kumar-yadav@users.noreply.github.com>
2017-08-13 11:08:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,9 +77,9 @@ app.get('/', function (req, res) {
 });
 
 var counter = 0;
-app.get('/counter' , function(req ,res){
+app.get('/counter' , function(req , res) {
     counter= counter + 1;
-    res.send(counter.tostring());
+    res.send(counter.toString());
 });
 
 app.get('/:articlename' , function (req,res) {
