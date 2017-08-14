b36994f55251e974a2da7a4069bcfc2a87840b80
rahul-kumar-yadav <30690192+rahul-kumar-yadav@users.noreply.github.com>
2017-08-14 08:49:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,22 @@ var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function()
 {
+     
     
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML= counter.toString();
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        if(request.readystate==XMLHttpRequest.DONE) {
+          
+          if(Request.Status==200)  {
+              var counter = request.response.Text;
+              var span = document.getElementById('count');
+              span.innerHTML= counter.toString();
+          }
+        } 
+    };
+    
+   request.open("GET",'http://rahulkumaryadav22.imad.hasura-app.io/counter' , true);
+   request.send(null);
+   
 };
