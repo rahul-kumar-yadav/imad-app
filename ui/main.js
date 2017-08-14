9980156cd3cebbe9fdefe9c2edc2cc5d41ec3e87
rahul-kumar-yadav <30690192+rahul-kumar-yadav@users.noreply.github.com>
2017-08-14 08:53:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,7 +17,7 @@ button.onclick = function()
         } 
     };
     
-   request.open("GET",'http://rahulkumaryadav22.imad.hasura-app.io/counter' , true);
+   request.open('GET','http://rahulkumaryadav22.imad.hasura-app.io/counter' , true);
    request.send(null);
    
 };
